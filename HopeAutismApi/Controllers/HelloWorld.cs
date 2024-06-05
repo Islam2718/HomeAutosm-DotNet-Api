520cb79bbc81a38dd66f929051d7c3962d1f9718
islam2718 <msialhn@gmail.com>
2024-06-06 04:40:21 +0600

    project created

---
+++ HopeAutismApi/Controllers/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HopeAutismApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorld : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ HopeAutismApi/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using HopeAutismApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HopeAutismApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> Users = new List<User>
+        {
+            new User { Id = 1, Username = "John Doe", Email = "john@example.com" },
+            new User { Id = 2, Username = "Jane Smith", Email = "jane@example.com"},
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return Users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            var user = Users[id];
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+
+        [HttpPost]
+        public ActionResult<User> Post(User user)
+        {
+            user.Id = Users.Count + 1;
+            Users.Add(user);
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var user = Users.FirstOrDefault(u => u.Id == id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            Users.Remove(user);
+            return NoContent();
+        }
+    }
+}
